d2482c0496badf4925bfcfd6a8359f568c0f9faf
Joseph Cook <110889269+Joseph-M-Cook@users.noreply.github.com>
2023-06-14 16:14:31 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,282 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.Net.Http.Headers;
+using System.IO;
+
+class Program
+{
+    // API Keys and Environments
+    static string OPENAI_API_KEY = "";
+    static string PINECONE_API_KEY = "";
+    static string PROJECT_NAME = "";
+
+    // Function to describe Pinecone index stats
+    static async Task GetIndexInfo(){
+        using (HttpClient client = new HttpClient())
+        {
+            try 
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri($"https://{PROJECT_NAME}/describe_index_stats"),
+                    Headers ={{ "accept", "application/json" },{ "Api-Key", PINECONE_API_KEY}}
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Index Info: {body}");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Failed to get index info...");
+                Console.WriteLine($"Request exception: {e.Message}");
+            }
+        }
+    }
+
+    // Function to clear all vectors from an index
+    static async Task ClearIndex(){
+        using (HttpClient client = new HttpClient())
+        {
+            try 
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri($"https://{PROJECT_NAME}/vectors/delete"),
+                    Headers ={{ "accept", "application/json" },{ "Api-Key", PINECONE_API_KEY}},
+                    Content = new StringContent("{\"deleteAll\":true}"){Headers ={ContentType = new MediaTypeHeaderValue("application/json")}}
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Index Cleared Successfully");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Failed to clear index...");
+                Console.WriteLine($"Request exception: {e.Message}");
+            }
+        }
+    }
+    
+    // Function to get embedding from OpenAI Embeddings API
+    static async Task<List<float>> GetEmbedding(string text){
+        using (HttpClient client = new HttpClient())
+        {
+            try 
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("https://api.openai.com/v1/embeddings"),
+                    Headers = { { "Authorization", $"Bearer {OPENAI_API_KEY}" },{ "accept", "application/json" }},
+                    Content = new StringContent(JsonSerializer.Serialize(new { input = text, model = "text-embedding-ada-002" }), Encoding.UTF8, "application/json")
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var jsonDocument = JsonDocument.Parse(body);
+                    var vec = jsonDocument.RootElement.GetProperty("data")[0].GetProperty("embedding");
+                    Console.WriteLine("Embedding Created Successfully");
+                     return JsonSerializer.Deserialize<List<float>>(vec.ToString());
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Failed to get embedding...");
+                Console.WriteLine($"Request exception: {e.Message}");
+                return new List<float>();
+            }
+        }
+    }
+
+    // Vector object
+    public class Vector{
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public List<float> Values { get; set; }
+        public Dictionary<string, int> Metadata { get; set; }
+
+        public Vector(string id, string text, List<float> values, Dictionary<string, int> metadata)
+        {
+            Id = id;
+            Text = text;
+            Values = values;
+            Metadata = metadata;
+        }
+    }
+
+    // Function to split text from file into chunks
+    public static List<string> SplitTextFileIntoChunks(string filePath){
+        string text = File.ReadAllText(filePath);
+        char[] delimiters = { '.' , '?', '!'};
+        string[] sentences = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            sentences[i] = sentences[i].Trim();
+        }
+
+        return new List<string>(sentences);
+    }
+
+    // Process chunks to vectors
+    static async Task<List<Vector>> ChunksToVectors(List<string> chunks){
+        List<Vector> vectors = new List<Vector>();
+
+        for (int i = 0; i < chunks.Count; i++)
+        {
+            Console.WriteLine($"Chunk {i}: {chunks[i].Substring(0, Math.Min(chunks[i].Length, 50))}");
+            List<float> testEmbedding = await GetEmbedding(chunks[i]);
+            string id = $"test_{i}";
+            var metadata = new Dictionary<string, int> { { "Test", i } };
+            Vector vector = new Vector(id, chunks[i], testEmbedding, metadata);
+            vectors.Add(vector); 
+        }
+        return vectors;
+    }
+
+    // Function to upsert a list of vectors in batches
+    static async Task UpsertVectors(List<Vector> vectors){
+        const int batchSize = 100;
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                for (int i = 0; i < vectors.Count; i += batchSize)
+                {
+                    List<Vector> batch = vectors.Skip(i).Take(batchSize).ToList();
+
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Post,
+                        RequestUri = new Uri($"https://{PROJECT_NAME}/vectors/upsert"),
+                        Headers ={{ "accept", "application/json" },{ "Api-Key", PINECONE_API_KEY }},
+                        Content = new StringContent(JsonSerializer.Serialize(new { vectors = batch.Select(v => new { id = v.Id, values = v.Values, metadata = v.Metadata }) }))
+                        {
+                            Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+                        }
+                    };
+
+                    using (var response = await client.SendAsync(request))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        var body = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(body);
+                    }
+                    Console.WriteLine("\nBatch Complete\n");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request exception: {e.Message}");
+            }
+        }
+    }
+    
+    // Function to perform Pinecone vector DB query and return list of IDs
+    static async Task<List<string>> PineconeQuery(Vector vector){
+        List<string> ids = new List<string>();
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri($"https://{PROJECT_NAME}/query"),
+                    Headers ={{ "accept", "application/json" },{ "Api-Key", PINECONE_API_KEY }},
+                    Content = new StringContent(JsonSerializer.Serialize(new
+                    {
+                        vector = vector.Values,
+                        includeMetadata = true,
+                        topK = 5
+                    }))
+                    {
+                        Headers =
+                        {
+                            ContentType = new MediaTypeHeaderValue("application/json")
+                        }
+                    }
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Query Embedded Successfully");
+                    //Console.WriteLine(body);
+
+                    // Parse the response JSON and extract the IDs
+                    var result = JsonSerializer.Deserialize<JsonDocument>(body);
+                    var matches = result.RootElement.GetProperty("matches");
+
+                    foreach (var match in matches.EnumerateArray())
+                    {
+                        var id = match.GetProperty("id").GetString();
+                        ids.Add(id);
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Failed to perform query...");
+                Console.WriteLine($"Request exception: {e.Message}");
+            }
+        }
+
+        return ids;
+    }
+
+    // Function that takes IDs and finds associated text
+    static async Task<List<string>> FindMatchingText(List<string> IDs, List<Vector> vectors){
+        List<string> matchingTexts = new List<string>();
+
+        foreach (var id in IDs)
+        {
+            // Find the corresponding vector object
+            Vector vector = vectors.FirstOrDefault(v => v.Id == id);
+
+            if (vector != null)
+            {
+                string text = vector.Text;
+                Console.WriteLine($"Context: {text.Substring(0, Math.Min(text.Length, 250))}");
+                matchingTexts.Add(text);
+            }
+        }
+
+        return matchingTexts;
+    }
+
+    // Main
+    static async Task Main(string[] args){
+        await GetIndexInfo();
+
+        string filePath = "testingtext.txt";
+        
+        List<string> chunks = SplitTextFileIntoChunks(filePath);
+        List<Vector> vectors = await ChunksToVectors(chunks);
+        
+        await UpsertVectors(vectors);
+
+        string user_query = "Tell me about Alex's report on the building.";
+        List<float> queryValues = await GetEmbedding(user_query);
+        Vector queryVector = new Vector("Query", user_query, queryValues, null);
+        List<string> matches = await PineconeQuery(queryVector);
+
+        List<string> matchingTexts = await FindMatchingText(matches, vectors);
+    }
+}
